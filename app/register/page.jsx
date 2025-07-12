a994dd93f6292f9cc6ee5deea2909cc0e3b35923
NetMoraly <mrakovlitvin@gmail.com>
2025-07-13 00:55:21 +0300

    Update page.jsx

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -82,7 +82,7 @@ export default function RegisterPage() {
     <>
       <Header />
       <div className="min-h-screen flex flex-col bg-gradient-to-br from-[#160029] to-[#6e1bb3] pt-[96px] px-4">
-        <div className="relative flex-grow flex items-center justify-center">
+       <div className="relative flex flex-1 items-center justify-center min-h-[calc(100vh-96px)]">
           <div className="absolute inset-0 z-0">
             <div className="absolute w-[300px] h-[300px] bg-pink-500/10 rounded-full blur-3xl top-10 left-10 animate-pulse" />
             <div className="absolute w-[250px] h-[250px] bg-purple-400/10 rounded-full blur-2xl bottom-20 right-20 animate-pulse" />
